332436d1770226717305e46e6fb1cf2e09b71266
Pritam Kumar <117884154+01pritam@users.noreply.github.com>
2025-02-03 11:09:48 +0530

    Update session13.cpp

--- Hashing-Prefix-Suffix/session13.cpp
+++ Hashing-Prefix-Suffix/session13.cpp
@@ -1,16 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    int n, target;
-    cin >> n >> target;
+    int n;
+    cin >> n;
     vector<int> a(n);
     unordered_map<int, int> mp;
+    int max_dist = -1;
     for (int i = 0; i < n; i++) {
         cin >> a[i];
-        if (mp.find(target - a[i]) != mp.end()) {
-            cout << mp[target - a[i]] << " " << i << endl;
-            return 0;
+        if (mp.find(a[i]) != mp.end()) {
+            max_dist = max(max_dist, i - mp[a[i]]);
         }
         mp[a[i]] = i;
     }
+    cout << max_dist << endl;
 }
